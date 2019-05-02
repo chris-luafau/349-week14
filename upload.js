580b873105d97d30b85a988863d09e50006ab0a8
hellochloe92 <45779918+hellochloe92@users.noreply.github.com>
2019-05-01 23:18:51 -0700

    Add files via upload

--- upload.js
+++ upload.js
@@ -1,42 +1,56 @@
 
-let s3Client = new AWS.S3({
-  endpoint: 'http://10.67.66.72:9000',
-  s3ForcePathStyle: true,
-  signatureVersion: 'v4',
-  accessKeyId: 'E46VPBJX0SN08957WXP4',
-  secretAccessKey: '+APqL7BGwF4DkL3dQZ4T3Qbnr1eyQYh5k1KqFaPL'
-});
-/*
-function upload() {
-  var caption = document.getElementById("caption");
-  var file = document.getElementById("myFile").files[0];
-  let reader = new FileReader();
-  reader.readAsBinaryString(file);
+(function(window) {
+  'use strict';
 
-  console.log(caption);
-  console.log(myFile);
-  s3Client.putObject({
-    Bucket: 'images',
-    Key: file.name,
-    Body: file,
-    ContentLength: file.size,
-    ContentType: file.type
-  }, function(err, data) {
-    console.log(err, data)
+
+
+    let s3Client = new AWS.S3({
+      endpoint: "http://127.0.0.1:9000",
+      s3ForcePathStyle: true,
+      signatureVersion: "v4",
+      accessKeyId: "BO6TYNVWT0ZU3JGRQPI7",
+      secretAccessKey: "uhrOBrLlL+qqfOmmceiLXS7I75isq2ZaEqPma+BL"
+
+    });
+
+    class UploadForm extends HTMLElement {
+      connectedCallback () {
+        this.innerHTML =
+        `<label>Caption: </label>
+          <input type="text" name="caption" id="caption"><br/>
+          <input type="file" id="myFile"><br/>
+          <input type="submit" value="Submit" id="submit"><br/>`;
+      }
+    }
+    customElements.define('upload-form', UploadForm);
+
+
+    $("#submit").on("click", function () {
+      //let file = $("#myFile").get(0);
+      let fileElement = document.getElementById("myFile");
+      let file = fileElement.files[0];
+      let caption = document.getElementById("caption");
+
+      s3Client.putObject({
+        Bucket: "images",
+        Key: file.name,
+        Body: file,
+        ContentLength: file.size,
+        ContentType: file.type
+      }, function(err, data) {
+          console.log(err, data)
+      });
+
+      $.ajax({
+        method: "POST",
+        url: "http://127.0.0.1:3000/images",
+        contentType: "application/json",
+        data: JSON.stringify({
+          caption: caption.value,
+          src: `http://127.0.0.1:9000/images/${file.name}`
+        })
+      });
+
   });
-}
-*/
-document.getElementById('up').onclick = function () {
-  var file = document.getElementById("myFile").files[0];
-  let reader = new FileReader();
-  reader.readAsBinaryString(file);
-  s3Client.putObject({
-    Bucket: 'cat',
-    Key: file.name,
-    Body: file,
-    ContentLength: file.size,
-    ContentType: file.type
-  }, function(err, data) {
-    console.log(err, data)
-  });
-}
+
+})(window);
